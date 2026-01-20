dc9a0d11ad9c1a86ca62d1081c9d0608a72324e2
unknown <ashishsindhav02@outlook.com>
2026-01-20 17:43:34 +0530

    minor name fix and minor UI change

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import BarChartAnalysis from './BarChartAnalysis';
+import WeeklyReturnsChart from './WeeklyReturnsChart';
 
 
 const Analysis = () => {
@@ -7,7 +7,7 @@ const Analysis = () => {
     <div className='flex w-full h-100 p-2'>
         <div className='flex relative w-full max-sm:h-fit md:h-full p-2 border border-emerald-500 rounded-lg bg-emerald-500/20'>
           <div className='max-sm:w-full max-sm:h-60 md:w-[40%] md:h-full'>
-            <BarChartAnalysis />
+            <WeeklyReturnsChart />
           </div>
         </div>
     </div>
