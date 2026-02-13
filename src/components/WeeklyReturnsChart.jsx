adc92fac2e922d2ebb65b69c6046f13b34c37ef4
unknown <ashishsindhav02@outlook.com>
2026-02-13 17:56:51 +0530

    minor change in bar chart ~office

--- src/components/WeeklyReturnsChart.jsx
+++ src/components/WeeklyReturnsChart.jsx
@@ -3,17 +3,17 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell } from 
 import CustomTooltip from './CustomTooltip';
 
 const weeklyData = [
-  { day: 'D1', pnl: 5000 },
-  { day: 'D2', pnl: -1200 },
-  { day: 'D3', pnl: 3500 },
-  { day: 'D4', pnl: -500 },
-  { day: 'D5', pnl: 8000 }
+  { day: 'DAY 1', pnl: 5000 },
+  { day: 'DAY 2', pnl: -1200 },
+  { day: 'DAY 3', pnl: 3500 },
+  { day: 'DAY 4', pnl: -500 },
+  { day: 'DAY 5', pnl: 8000 }
 ];
 
 const WeeklyReturnsChart = () => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <BarChart data={weeklyData}>
+      <BarChart data={weeklyData} margin={{top: 20, right: 0, bottom: 0, left: 0}}>
         
         <XAxis
           dataKey="day"
@@ -27,7 +27,7 @@ const WeeklyReturnsChart = () => {
           tick={{ fontSize: 12, fill: '#64748b' }} 
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => `₹${value}`}
+          tickFormatter={(value) => `₹${value || "Not Traded"}`}
         />
         
         {/* <Tooltip 
@@ -36,7 +36,7 @@ const WeeklyReturnsChart = () => {
         /> */}
         <Tooltip cursor={{ fill: 'rgba(128,128,128,0.05)' }} content={<CustomTooltip />} />
 
-        <Bar barSize="50" dataKey="pnl" fill="#22c55e">
+        <Bar barSize="40" dataKey="pnl" fill="#22c55e">
           
           {weeklyData.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={entry.pnl >= 0 ? '#22c55e' : '#ef4444'} />
